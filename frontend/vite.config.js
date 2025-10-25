7c2f7b3fa82099d6b32cbe3af9bf29437d9fb997
himanshu45-tech <himanshu.prpwebs@gmail.com>
2025-10-25 14:36:04 +0530

    chnage url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, host: true }
+  server: { port: 5173, host: true },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
 })
